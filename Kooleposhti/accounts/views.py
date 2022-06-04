fed76d27ea1832783220c5b74e16f1bd48a68ff2
Emadmousavi <emadmoosavi79@gmail.com>
2022-06-04 23:28:19 +0430

    [fix] fix serializer error

--- Kooleposhti/accounts/views.py
+++ Kooleposhti/accounts/views.py
@@ -209,7 +209,7 @@ class MyTokenObtainPairView(TokenViewBase):
     Takes a set of user credentials and returns an access and refresh JSON web
     token pair to prove the authentication of those credentials.
     """
-    serializer_class = serializers.F
+    serializer_class = serializers.MyTokenObtainPairSerializer
 
 
 class UserResetPassword(GenericViewSet):
